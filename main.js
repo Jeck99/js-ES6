d7a039c9cb58b41dea24e1cc87f8a3cc51a6b9b4
Jeck99 <Jeck99@gmail.com>
2023-09-11 15:28:43 +0300

    objects

--- main.js
+++ main.js
@@ -1,16 +1,56 @@
-var myVar = 9; //hoisting
+let firstNum = 9;
+let secondNum = 5;
+const myMath = {
+    firstNum, //firstNum:9
+    secondNum, //secondNum:5
+    sum() {
+        return this.firstNum + this.secondNum;
+    },
+    printSum() {
+        console.log(this.sum());
 
-let myLet = 34 //without hoisting
-
-const MY_CONST = "hello" //without hoisting + permeanet
-// MY_CONST = "jacob"; X
-const CONST_ARRAY = [34,46,,45,35]
-// CONST_ARRAY = [56775,8,87,4,34] X
-CONST_ARRAY[i] = 534;
-
-const USER_OBJECT = {
-    fName:"jacob",
-    age:34
+    }
 }
-USER_OBJECT= {}
-USER_OBJECT.age = 56;
+myMath.firstNum;
+myMath.secondNum;
+myMath.printSum();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var myVar = 9; //hoisting
+
+// let myLet = 34 //without hoisting
+
+// const MY_CONST = "hello" //without hoisting + permeanet
+// // MY_CONST = "jacob"; X
+// const CONST_ARRAY = [34, 46, , 45, 35]
+// // CONST_ARRAY = [56775,8,87,4,34] X
+// CONST_ARRAY[i] = 534;
+
+// const USER_OBJECT = {
+//     fName: "jacob",
+//     age: 34
+// }
+// USER_OBJECT = {}
+// USER_OBJECT.age = 56;
