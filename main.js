0a299567b8191cce8edb6985ad4d12f1102a8305
Jeck99 <Jeck99@gmail.com>
2023-09-11 14:51:41 +0300

    var let const

---
+++ main.js
@@ -0,0 +1,16 @@
+var myVar = 9; //hoisting
+
+let myLet = 34 //without hoisting
+
+const MY_CONST = "hello" //without hoisting + permeanet
+// MY_CONST = "jacob"; X
+const CONST_ARRAY = [34,46,,45,35]
+// CONST_ARRAY = [56775,8,87,4,34] X
+CONST_ARRAY[i] = 534;
+
+const USER_OBJECT = {
+    fName:"jacob",
+    age:34
+}
+USER_OBJECT= {}
+USER_OBJECT.age = 56;
